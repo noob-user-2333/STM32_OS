6533515586e1079853af3c693e64d6aa440b1c49
noob-user-2333 <66577944+noob-user-2333@users.noreply.github.com>
2021-03-31 15:47:06 +0800

    Add files via upload

--- thread_manage/os_thread_schedule.s
+++ thread_manage/os_thread_schedule.s
@@ -1,5 +1,6 @@
 		IMPORT os_thread_current_ptr
 		IMPORT task_switch_context
+		IMPORT os_state
 	AREA  ||.text||,CODE,READONLY
 	PRESERVE8
 	EXPORT os_thread_schedule
@@ -17,9 +18,20 @@ os_thread_schedule
 	CPSIE F
     DSB                                             ; Complete all memory accesses
     ISB
-	SVC 0
+	MOV     r0, #0x10000000                         ; Load PENDSVSET bit
+    MOV     r1, #0xE000E000                         ; Load NVIC base
+    STR     r0, [r1, #0xD04]                        ; Set PENDSVBIT in ICSR
+;	SVC 0
+	DSB
+	ISB
 	NOP
 	NOP
+os_wait_here
+	NOP
+	NOP
+	NOP
+	B os_wait_here
+	
 	EXPORT SVC_Handler
 SVC_Handler	
 	ldr	r3, =os_thread_current_ptr	
@@ -32,20 +44,23 @@ SVC_Handler
 	msr	basepri, r0
 	orr r14, #0xd
 	bx r14
-	
-os_wait_here
 	B os_wait_here
 
 	EXPORT PendSV_Handler
 PendSV_Handler
+	LDR r0,=os_state
+	LDR r1,[r0]
+	CBZ	r1,os_first_start
+
 	MRS r0,psp
-	isb
+	ISB
 	LDR r3,=os_thread_current_ptr
 	LDR r2,[r3]
 	
 	STMDB r0!,{r4-r11}
 	STR   r0,[r2,#12]
-	
+
+
 	STMDB sp!,{r3,r14}
 	MOV	  r0,#0xE0
 	MSR   basepri,r0
@@ -61,9 +76,28 @@ PendSV_Handler
 	LDR r0,[r1,#12]
 	LDMIA r0!,{r4-r11}
 	MSR	 psp,r0
+	
 	ISB
 	BX	 r14
 	NOP
+	NOP
+	
+os_first_start
+	ldr	r3, =os_thread_current_ptr	
+	ldr r1, [r3]			
+	ldr r0, [r1,#12]			
+	ldmia r0!, {r4-r11}		
+	msr psp, r0				
+	isb
+	mov r0, #0
+	msr	basepri, r0
+	MOV	r2,#1
+	LDR r1,=os_state
+	STR	r2,[r1]
+	orr r14, #0xd
+	bx r14
+	B os_wait_here
+	
 	ALIGN
 ;	LTORG
 	END
