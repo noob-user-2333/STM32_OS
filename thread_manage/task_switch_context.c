927a72d5e4f922f5f219d35eb4e163b3cc85312b
noob-user-2333 <66577944+noob-user-2333@users.noreply.github.com>
2021-03-31 16:34:02 +0800

    将内核进程设置为空闲进程

--- thread_manage/task_switch_context.c
+++ thread_manage/task_switch_context.c
@@ -2,16 +2,19 @@
 
 extern OS_THREAD *os_thread_current_ptr;
 extern OS_THREAD *os_thread_execute_ptr;
+extern OS_THREAD *os_thread_kernel_thread_ptr;
 
 void task_switch_context()
 {
-	
+	//没有任务可运行则运行内核进程
+	if(os_thread_current_ptr == NULL && os_thread_execute_ptr == NULL)
+	{
+			os_thread_kernel_thread_ptr->os_thread_ready_next = os_thread_kernel_thread_ptr;
+			os_thread_kernel_thread_ptr->os_thread_ready_previous = os_thread_kernel_thread_ptr;
+			os_thread_execute_ptr = os_thread_kernel_thread_ptr;
+	}
 	if(os_thread_execute_ptr)
 	{
-		if(os_thread_current_ptr->state != THREAD_READY)
-		{
-			os_thread_remove_from_list(&os_thread_current_ptr,os_thread_current_ptr);	
-		}
 		OS_THREAD *thread_ptr = os_thread_execute_ptr;
 		os_thread_remove_from_list(&os_thread_execute_ptr,thread_ptr);
 		os_thread_add_to_list(&os_thread_current_ptr,thread_ptr);
@@ -19,19 +22,31 @@ void task_switch_context()
 	}
 	else
 	{
-		if(os_thread_current_ptr->state != THREAD_READY)
+		
+		if(os_thread_current_ptr->state != READY)
 		{
 			os_thread_remove_from_list(&os_thread_current_ptr,os_thread_current_ptr);	
 		}
 		else
 		{
+			
 			os_thread_current_ptr = os_thread_current_ptr->os_thread_ready_next;
 		}
 	}
 	
 	
 }
+void task_switch_to_kernel_thread()
+{
+	if(os_thread_current_ptr != os_thread_kernel_thread_ptr)
+	{
+		os_thread_kernel_thread_ptr->os_thread_ready_next = 		os_thread_current_ptr;
+		os_thread_kernel_thread_ptr->os_thread_ready_previous = os_thread_current_ptr->os_thread_ready_previous;
 
+		os_thread_current_ptr = os_thread_kernel_thread_ptr;
+		os_thread_current_ptr->time_slice_current = os_thread_current_ptr->time_slice_new;
+	}
+}
 
 
 void SysTick_Handler(void)
